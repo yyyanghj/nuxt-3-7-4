2f37a24e9ec50583fac666db763d3eeedb92b541
Silvio Eckl <silvio@whitespace.no>
2023-09-26 19:32:29 +0200

    Add modules to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: { preset: 'vercel-edge' },
   devtools: { enabled: true },
-  nitro: { preset: 'vercel-edge' }
+  experimental: {
+    componentIslands: true
+  },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss'
+  ],
+  googleFonts: {
+    families: {
+      'Plus Jakarta Sans': [400]
+    }
+  },
+  tailwindcss: {
+    viewer: false
+  },
+  vue: {
+    compilerOptions: {
+      isCustomElement: tag => tag === 'calenso-booking'
+    }
+  }
 })
